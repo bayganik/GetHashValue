da1ac4936da7873adc19b6a9f1101c8138e4bf7f
Kamran <bayganik@gmail.com>
2018-01-18 12:35:31 -0500

    Initial commit

---
+++ GetHashValue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> hashes = Enumerable.Range(0, 1000).Select(m =>
+                         Convert.ToBase64String(Guid.NewGuid().ToByteArray())
+                         .Substring(0, 22).Replace("/", "_").Replace("+", "-"))
+                         .Where(m => !m.Contains("-")).Where(m => !m.Contains("_"))
+                         .ToList();
+
+            string str = string.Join(Environment.NewLine, hashes);
+
+            Console.WriteLine(str);
+            Console.ReadLine();
+
+            //ljXNqG0vS0G9dr1Z06zBVA                priority       
+            //dNY4GoOMW0yofBMZfpOAhw
+        }
+    }
+}
